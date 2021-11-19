e0a5740bb8cbbb1f7a784af6d7ce9ae2815f20ac
realroot <taste1729@gmail.com>
2021-11-19 18:14:06 +0900

    'stack5'

---
+++ stack/src/B4949.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class B4949 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		for (int i = 0; i < s.length(); i++) {
+			Stack<Character> stack = new Stack<Character>();
+			if (s.charAt(i) == '[' || s.charAt(i)== '(') {
+				char temp = s.charAt(i);
+				stack.push(temp);
+			} else if (s.charAt(i) == ']') {
+				if (stack.size() != 0 && stack.peek() == '[') {
+					stack.pop();
+				} else {
+					System.out.println("no");
+					break;
+				}
+			} else if (s.charAt(i) == ')') {
+				if (stack.size() != 0 && stack.peek() == '(') {
+					stack.pop();
+				} else {
+					System.out.println("no");
+					break;
+				}
+			} else if (s.charAt(i) == '.') {
+				if (stack.size() == 0) {
+					break;
+				} else {
+					System.out.println("no");
+					break;
+				}
+			}
+		}
+	}
+}
