8076af0cedb87e316b990d9ca0c979f6388f5d85
realroot <taste1729@gmail.com>
2021-11-22 19:58:49 +0900

    'stack7'

--- stack/src/stack/B10799.java
+++ stack/src/stack/B10799.java
@@ -1,5 +1,10 @@
 package stack;
 
-public class B10799 {
+import java.io.*;
+import java.util.*;
 
+public class B10799 {
+  public static void main(String[] args) throws IOException {
+	  
+  }
 }
--- stack/src/stack/B2504.java
+++ stack/src/stack/B2504.java
@@ -7,48 +7,48 @@ public class B2504 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = sc.nextLine();
-	    Stack<Value> stack = new Stack<>();
-	    
+		Stack<Value> stack = new Stack<>();
+
 		int sum = 0;
-		for(int i =0; i<str.length();i++) {
-	      char c = str.charAt(i);
-	      if(c == '(' || c == '[') {
-	    	  Value temp = new Value(c,0);
-	    	  stack.push(temp);
-	      }else if(c == ')' && !stack.isEmpty() && stack.peek().c == '(') {
-	    	  if(stack.peek().value == 0) {
-	        	 sum = sum + 2;
-	         }else {
-	        	 sum = sum*2;
-	         }
-	    	  stack.pop();
-	    	  if(!stack.isEmpty()) {
-	    		  stack.peek().value = 1;
-	    	  }
-	      }else if(c == ']' && !stack.isEmpty() && stack.peek().c == '[') {
-	    	  if(stack.peek().value == 0) {
-		        	 sum = sum + 3;
-		         }else {
-		        	 sum = sum*3;
-		         } 
-	    	  stack.pop();
-	    	  if(!stack.isEmpty()) {
-	    		  stack.peek().value = 1;
-	    	  }
-	      }else {
-	    	  sum =0; 
-	    	  break;
-	      }
+		for (int i = 0; i < str.length(); i++) {
+			char c = str.charAt(i);
+			if (c == '(' || c == '[') {
+				Value temp = new Value(c, 0);
+				stack.push(temp);
+			} else if (c == ')' && !stack.isEmpty() && stack.peek().c == '(') {
+				if (stack.peek().value == 0) {
+					sum = sum + 2;
+				} else {
+					sum = sum * 2;
+				}
+				stack.pop();
+				if (!stack.isEmpty()) {
+					stack.peek().value = 1;
+				}
+			} else if (c == ']' && !stack.isEmpty() && stack.peek().c == '[') {
+				if (stack.peek().value == 0) {
+					sum = sum + 3;
+				} else {
+					sum = sum * 3;
+				}
+				stack.pop();
+				if (!stack.isEmpty()) {
+					stack.peek().value = 1;
+				}
+			} else {
+				sum = 0;
+				break;
+			}
 		}
 		System.out.println(sum);
 	}
 }
 
-class Value{
+class Value {
 	char c;
 	int value;
-	
-	Value(char c,int value){
+
+	Value(char c, int value) {
 		this.c = c;
 		this.value = value;
 	}
