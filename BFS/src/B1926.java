c45fed8b7777dfcca07ba71ce5bc7ffcaf35daa9
realroot <taste1729@gmail.com>
2021-11-27 22:43:17 +0900

    'dfs1'

---
+++ BFS/src/B1926.java
@@ -0,0 +1,67 @@
+
+import java.util.*;
+import java.io.*;
+
+public class B1926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int[][] arr = new int[n][m];
+		Stack<bfs> stack = new Stack<>();
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+
+		int max = 0;
+		int cnt = 0;
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (arr[i][j] == 1) {
+					bfs temp = new bfs(i, j);
+					stack.push(temp);
+					int count = 0;
+					while (!stack.isEmpty()) {
+						int x = stack.peek().x;
+						int y = stack.peek().y;
+						stack.pop();
+						if (x + 1 != n && arr[x + 1][y] == 1) {
+							bfs temp2 = new bfs(x + 1, y);
+							stack.push(temp2);
+						} else if (x - 1 != -1 && arr[x - 1][y] == 1) {
+							bfs temp2 = new bfs(x - 1, y);
+							stack.push(temp2);
+						} else if (y + 1 != m && arr[x][y + 1] == 1) {
+							bfs temp2 = new bfs(x, y + 1);
+							stack.push(temp2);
+						} else if (y - 1 != -1 && arr[x][y - 1] == 1) {
+							bfs temp2 = new bfs(x, y - 1);
+							stack.push(temp2);
+						}
+						arr[x][y] = 0;
+						count++;
+					}
+					if (max < count)
+						max = count;
+					cnt++;
+				}
+			}
+		}
+		System.out.println(cnt);
+		System.out.println(max);
+	}
+}
+
+class bfs {
+	int x;
+	int y;
+
+	bfs(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
---
+++ BFS/src/B2178.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class B2178 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] arr = new int[n][m];
+		int[] dx = { 1, 0, -1, 0 };
+		int[] dy = { 0, 1, 0, -1 };
+		boolean[][] visited=new boolean[n][m];
+
+		Queue<index> queue = new LinkedList<>();
+		for (int i = 0; i < n; i++) {
+			String temp = br.readLine();
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = (int) temp.charAt(j) - '0';
+				visited[i][j] = false;
+			}
+		}
+
+		queue.add(new index(0, 0));
+		visited[0][0] = true;
+		
+		while (!queue.isEmpty()) {
+			index now = queue.remove();
+			for (int i = 0; i < dx.length; i++) {
+				int xx = now.x + dx[i];
+				int yy = now.y + dy[i];
+				if (xx >= 0 && yy >= 0 && xx < n && yy < m) {
+					if (arr[xx][yy] != 0 && !visited[xx][yy]) {
+						visited[xx][yy] = true;
+						arr[xx][yy] = arr[now.x][now.y] + 1;
+						queue.add(new index(xx, yy));
+					}
+				}
+			}
+		}
+		System.out.println(arr[n-1][m-1]);
+	}
+}
+
+class index {
+	int x, y;
+
+	index(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
