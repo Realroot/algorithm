e0c68ea3e103fd706d3d97821b9232d2b2eacaaa
realroot <taste1729@gmail.com>
2021-12-06 21:04:31 +0900

    'dfs4'

---
+++ BFS/src/BFS/B2583.java
@@ -0,0 +1,68 @@
+package BFS;
+
+import java.util.*;
+import java.io.*;
+
+public class B2583 {
+	static int M, N, K;
+	static int[] dx = { 0, -1, 0, 1 };
+	static int[] dy = { 1, 0, -1, 0 };
+	static int[][] arr;
+    static boolean[][] visited;
+    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		arr = new int[M][N];
+		visited = new boolean[M][N];
+		
+		for (int i = 0; i < K; i++) {
+			st = new StringTokenizer(br.readLine());
+			int startx = Integer.parseInt(st.nextToken());
+			int starty = Integer.parseInt(st.nextToken());
+			int endx = Integer.parseInt(st.nextToken());
+			int endy = Integer.parseInt(st.nextToken());
+			for (int j = starty; j < endy; j++) {
+				for (int k = startx; k < endx; k++) {
+					arr[j][k] = 1;
+				}
+			}	
+		}
+        Queue<index> queue = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        int cnt = 0;
+        for(int i=0;i<M;i++) {
+        	for(int j=0;j<N;j++) {
+              if(arr[i][j] == 0) {
+            	  queue.add(new index(i,j));
+            	  int width = 1;
+            	  while(!queue.isEmpty()) {
+            		  index now = queue.remove();
+            		  arr[now.x][now.y] = 1;
+            		  width++;
+            		  for(int k=0;k<4;k++) {
+            			  int xx = now.x +dx[k];
+            			  int yy = now.y +dy[k];
+            			  if(xx>=0 && yy>=0 && xx<M && yy<N) {
+            				  if(arr[xx][yy] ==0) {
+            					  queue.add(new index(xx,yy));
+            				  }
+            			  }
+            		  }
+            	  }
+            	  list.add(width);
+            	  cnt++;
+              }
+        	}
+        }
+        System.out.println(cnt);
+        for(int i =0;i<list.size();i++) {
+        	System.out.println(list.get(i));
+        }
+        
+	}
+}
