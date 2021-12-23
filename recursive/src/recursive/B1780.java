3a3744de39403f65ef9bef3b32a21beb20cf3605
realroot <taste1729@gmail.com>
2021-12-23 14:46:27 +0900

    'recursive4'

--- recursive/src/recursive/B1780.java
+++ recursive/src/recursive/B1780.java
@@ -5,7 +5,9 @@ import java.util.*;
 
 public class B1780 {
    
-	
+	static int cnt0 =0;
+	static int cnt1 =0;
+	static int cnt2 =0;
 	public static  void main(String[] args) throws IOException{
 	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	   StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,11 +23,48 @@ public class B1780 {
 	   }
 	   
 	   cut(N,arr);
+	   System.out.println(cnt0);
+	   System.out.println(cnt1);
+	   System.out.println(cnt2);
    }
 	
-	static int cut(int n,int[][] arr) {
+	static void cut(int n,int[][] arr) {
+		int temp = arr[0][0];
+		boolean check = false;
 		
-		return 1;
+	   Loop1 :
+	   for(int i=0;i<arr.length;i++) {
+		   Loop2 :
+		   for(int j=0;j<arr[0].length;j++) {
+			   if(arr[i][j] != temp) {
+				   check =true;
+				   break Loop1;
+			   }
+		   }
+	   }
+		
+		if(check) {		
+			for(int i =0;i<3;i++) {
+				for(int j=0;j<3;j++) {
+					int[][] tempArr = new int[n/3][n/3];
+					for(int ii=0;ii<n/3;ii++) {
+						for(int jj = 0;jj<n/3;jj++) {
+						  tempArr[ii][jj] = arr[i*n/3+ii][j*n/3+jj];
+						}
+					}
+					cut(n/3,tempArr);
+				}
+			}
+		}else {
+			if(temp == -1) {
+				cnt0++;
+			}else if(temp == 0) {
+				cnt1++;
+			}else {
+				cnt2++;
+			}
+		}
+	   
+	   
 	}
-	
 }
