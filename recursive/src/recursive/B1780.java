28b1efc1c5d41f39b8cbcea85cd9210ba5d63868
realroot <taste1729@gmail.com>
2021-12-23 09:17:43 +0900

    'recursive3'

--- recursive/src/recursive/B1780.java
+++ recursive/src/recursive/B1780.java
@@ -1,5 +1,31 @@
 package recursive;
 
-public class B1780 {
+import java.io.*;
+import java.util.*;
 
+public class B1780 {
+   
+	
+	public static  void main(String[] args) throws IOException{
+	   BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	   StringTokenizer st = new StringTokenizer(br.readLine());
+	   
+	   int N = Integer.parseInt(st.nextToken());
+	   int[][] arr = new int[N][N];
+	   
+	   for(int i =0;i<N;i++) {
+		   st = new StringTokenizer(br.readLine());
+		   for(int j =0;j<N;j++) {
+			  arr[i][j] = Integer.parseInt(st.nextToken()); 
+		   }
+	   }
+	   
+	   cut(N,arr);
+   }
+	
+	static int cut(int n,int[][] arr) {
+		
+		return 1;
+	}
+	
 }
