44749e5248b69708f09417da33ca179a34e95909
realroot <taste1729@gmail.com>
2021-12-29 16:28:35 +0900

    'backtracking4'

---
+++ backtracking/src/backtracking/B15650.java
@@ -0,0 +1,35 @@
+package backtracking;
+
+import java.util.*;
+import java.io.*;
+
+public class B15650 {
+	static int N, M;
+	static int[] arr;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[M];
+				
+		sequence(0,1);
+	}
+
+	static void sequence(int k,int n) {
+		if(k==M) {
+			for(int i =0;i<arr.length;i++) {
+				System.out.print(arr[i]+" ");
+			}
+			System.out.println("");
+			return ;
+		}
+		
+		for(int i=n;i<=N;i++) {
+			
+				arr[k] =i;
+				sequence(k+1,i+1);
+			
+		}
+	}
+}
